eb1a6b6dbfffae383021aaf7e952b4454ec93e87
carl.berglund <redguyshrug@gmail.com>
2019-10-24 11:17:09 +0200

    lektion slut

--- src/Kalkylatorn.java
+++ src/Kalkylatorn.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 
 public class Kalkylatorn {
     public static void main (String[] args) {
-        
+        {
+        int ekvation = Integer.parseInt(JOptionPane.showInputDialog(null, "Grad av ekvation?"));
+            int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
+            int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på b?"));
+            int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
+            int potens = Integer.parseInt(JOptionPane.showInputDialog(null, "Potens?"));
+        }
+        if (ekvation =0) {
+            a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
+        }
+        else if (ekvation =1){
+            a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
+            b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på b?"));
+        }
+        else if (ekvation =2){
+            a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
+            b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
+            c = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
+        }
+
         }
     }
