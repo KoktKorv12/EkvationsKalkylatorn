de82513f9823c645293816317fa3a6826798609c
carl.berglund <redguyshrug@gmail.com>
2019-10-25 11:16:25 +0200

    lektion2slut

--- src/Kalkylatorn.java
+++ src/Kalkylatorn.java
@@ -2,26 +2,26 @@ import javax.swing.*;
 import java.util.Scanner;
 
 public class Kalkylatorn {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         {
-        int ekvation = Integer.parseInt(JOptionPane.showInputDialog(null, "Grad av ekvation?"));
+            int ekvation = Integer.parseInt(JOptionPane.showInputDialog(null, "Ange ekvationens grad?"));
             int a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
             int b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på b?"));
             int c = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
             int potens = Integer.parseInt(JOptionPane.showInputDialog(null, "Potens?"));
-        }
-        if (ekvation =0) {
-            a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
-        }
-        else if (ekvation =1){
-            a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
-            b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på b?"));
-        }
-        else if (ekvation =2){
-            a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
-            b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
-            c = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
-        }
+
+            if (ekvation == 0) {
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "a?"));
+                JOptionPane.showMessageDialog(null, a);
+            } else if (ekvation == 1) {
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på b?"));
+            } else if (ekvation == 2) {
+                a = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på a?"));
+                b = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på b?"));
+                c = Integer.parseInt(JOptionPane.showInputDialog(null, "Värde på c?"));
+            }
 
         }
     }
+}
